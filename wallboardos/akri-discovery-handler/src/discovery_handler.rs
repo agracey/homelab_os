3d58f7084ce01c6871a1a3c3cbe8c8572bc15a21
Andrew Gracey <andrew.gracey@suse.com>
2022-03-15 07:39:16 -0700

    working

---
+++ wallboardos/akri-discovery-handler/src/discovery_handler.rs
@@ -0,0 +1,37 @@
+use akri_discovery_utils::discovery::{
+    v0::{discovery_handler_server::DiscoveryHandler, Device, DiscoverRequest, DiscoverResponse},
+    DiscoverStream,
+};
+use async_trait::async_trait;
+use tokio::sync::mpsc;
+use tonic::{Response, Status};
+use std::fs;
+
+
+pub struct DiscoveryHandlerImpl {
+    register_sender: tokio::sync::mpsc::Sender<()>,
+}
+
+impl DiscoveryHandlerImpl {
+    pub fn new(register_sender: tokio::sync::mpsc::Sender<()>) -> Self {
+        DiscoveryHandlerImpl { register_sender }
+    }
+}
+
+#[async_trait]
+impl DiscoveryHandler for DiscoveryHandlerImpl {
+    type DiscoverStream = DiscoverStream;
+    async fn discover(
+        &self,
+        request: tonic::Request<DiscoverRequest>,
+    ) -> Result<Response<Self::DiscoverStream>, Status> {
+        unimplemented!("add your discovery logic here");
+
+        // let paths = fs::read_dir("./").unwrap();
+
+        // for path in paths {
+        //     println!("Name: {}", path.unwrap().path().display())
+        // }
+
+    }
+}
